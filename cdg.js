b1f429f943fd0fc1bc5716735a4182035a786820
AnnefranceFBC <71523877+AnnefranceFBC@users.noreply.github.com>
2021-09-22 17:23:18 -0300

    finish four class

---
+++ cdg.js
@@ -0,0 +1,6 @@
+var listaFilmes = ["https://cdn.falauniversidades.com.br/wp-content/uploads/2021/04/22171459/Shrek.jpg", "https://dinheirama.com/wp-content/uploads/2008/02/a-procura-da-felicidade.jpg", "https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/07/vingadores-ultimato-1200x900-1.jpg"];
+
+
+for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src =" + listaFilmes[i] + ">");
+}
