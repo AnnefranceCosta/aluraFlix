57f5c124a292d62235ee23a92c1b6dc14925741c
AnnefranceFBC <71523877+AnnefranceFBC@users.noreply.github.com>
2021-09-22 19:31:57 -0300

    finish project

--- cdg.js
+++ cdg.js
@@ -1,6 +1,15 @@
-var listaFilmes = ["https://cdn.falauniversidades.com.br/wp-content/uploads/2021/04/22171459/Shrek.jpg", "https://dinheirama.com/wp-content/uploads/2008/02/a-procura-da-felicidade.jpg", "https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/07/vingadores-ultimato-1200x900-1.jpg"];
+function adicionarFilme () {
+    var filmeFavorito = document.getElementById("filme").value;
+    if(filmeFavorito.endsWith(".jpg")){
+            listarFilmesNaTela();
+        }else{
+           console.error("Endereço de filme inválido");
+        }  
+    document.getElementById("filme").value = "";
+}
 
-
-for (var i = 0; i < listaFilmes.length; i++) {
-    document.write("<img src =" + listaFilmes[i] + ">");
+function listarFilmesNaTela(filme) {
+    var elementoFilmeFavorito = "<img src=" + filmeFavorito + ">";
+    var elementoListaFilmes = document.getElementById("listaFilmes");
+     elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito;
 }
